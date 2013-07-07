1191159dc0e5ea15c500679e35168a500ff5bf36
Travis Roberts <traviskroberts@gmail.com>
2013-07-07 14:23:15 -0500

    Moved email config to application.rb.

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,16 @@ module Nshvll
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    ActionMailer::Base.smtp_settings = {
+        :port           => '587',
+        :address        => 'smtp.mandrillapp.com',
+        :user_name      => ENV['MANDRILL_USERNAME'],
+        :password       => ENV['MANDRILL_APIKEY'],
+        :domain         => 'heroku.com',
+        :authentication => :plain
+    }
+    ActionMailer::Base.delivery_method = :smtp
   end
 end
 

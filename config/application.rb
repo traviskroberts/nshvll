4c5f0b5d10f00e3e5527a2ce6861918437fc09f2
Travis Roberts <traviskroberts@gmail.com>
2016-02-28 12:32:35 -0600

    Replace Mandrill with Mailgun for SMTP email

--- config/application.rb
+++ config/application.rb
@@ -18,12 +18,12 @@ module Nshvll
     config.filter_parameters += [:password]
 
     ActionMailer::Base.smtp_settings = {
-        :port           => '587',
-        :address        => 'smtp.mandrillapp.com',
-        :user_name      => ENV['MANDRILL_USERNAME'],
-        :password       => ENV['MANDRILL_APIKEY'],
-        :domain         => 'heroku.com',
-        :authentication => :plain
+      :port           => ENV['MAILGUN_SMTP_PORT'],
+      :address        => ENV['MAILGUN_SMTP_SERVER'],
+      :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+      :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+      :domain         => 'nshvll.heroku.com',
+      :authentication => :plain,
     }
     ActionMailer::Base.delivery_method = :smtp
   end
